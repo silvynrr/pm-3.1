830d879ba363771489ff5e7bc471a7a4cee406bc
silvynrr <141999356+silvynrr@users.noreply.github.com>
2023-10-02 14:23:42 +0700

    Add files via upload

---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+
+function profile(namaDepan, namaBelakang, email) {
+    console.log("Nama : " + namaDepan + " "+ namaBelakang)
+    console.log("Email : "+ email)
+}
+
+profile("silvy","nur agustin","silvy@gmail.com")
